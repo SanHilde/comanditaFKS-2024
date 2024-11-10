cc357aae3ebcb1981a49e9ce812cd47898467276
Fabiola <suarezfabiola17@gmail.com>
2024-11-10 20:54:59 -0300

    Se agrega servicio de enviar notificaciones con one signal

---
+++ android/app/src/main/java/io/ionic/starter/ApplicationClass.java
@@ -0,0 +1,26 @@
+package io.ionic.starter;
+import android.app.Application;
+import com.onesignal.OneSignal;
+import com.onesignal.debug.LogLevel;
+import com.onesignal.Continue;
+
+public class ApplicationClass extends Application {
+
+  // NOTE: Replace the below with your own ONESIGNAL_APP_ID
+  private static final String ONESIGNAL_APP_ID = "6c7862cc-4153-40fa-aa8b-ceb9d42d6dd4";
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Verbose Logging set to help debug issues, remove before releasing your app.
+    OneSignal.getDebug().setLogLevel(LogLevel.VERBOSE);
+
+    // OneSignal Initialization
+    OneSignal.initWithContext(this, ONESIGNAL_APP_ID);
+
+    // requestPermission will show the native Android notification permission prompt.
+    // NOTE: It's recommended to use a OneSignal In-App Message to prompt instead.
+    OneSignal.getNotifications().requestPermission(false, Continue.none());
+  }
+}
--- src/app/services/notificaciones.service.ts
+++ src/app/services/notificaciones.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificacionesService {
+  private appId: string = '6c7862cc-4153-40fa-aa8b-ceb9d42d6dd4';
+  private apiKey: string = 'YWFiMGQwZDktYmFhNi00MTQ0LThiYzAtYzkxYTk3YjZkZDk1';
+  private apiUrl: string = 'https://onesignal.com/api/v1/notifications';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  enviarNotificacion(titulo: string, mensaje: string, usuarioId: string) {
+    // const headers = new HttpHeaders({
+    //   'Content-Type': 'application/json',
+    //   Authorization: `Basic ${this.apiKey}`,
+    // });
+    // const body = {
+    //   app_id: this.appId,
+    //   include_external_user_ids: [usuarioId], // ID del usuario al que se enviará la notificación
+    //   headings: { en: titulo },
+    //   contents: { en: mensaje },
+    // };
+    // return this.http.post(this.apiUrl, body, { headers });
+  }
 }
